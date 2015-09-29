5bbcd3f1cf3e8b33267cf852d5f6e04ad619b49a
Ethan Gibson <me@ethangibson.com>
2015-09-29 01:32:10 +0100

    Initial commit

---
+++ src/com/impression11/helloworld/main.java
@@ -0,0 +1,27 @@
+package com.impression11.helloworld;
+
+import java.util.Scanner;
+
+/**
+ * Created by Ethan on 28/09/15.
+ */
+public class main {
+
+    public static void main(String[] args) {
+
+        int hello = 1;
+        hello++;
+        String jello = "hello there";
+        System.out.println(hello);
+
+        Scanner sc = new Scanner(System.in);
+        //nextInt for numbers
+        //next for string
+        //rememeber to show it breaking for wrong input types!
+        int hello2 = sc.nextInt();
+        System.out.println(hello2);
+
+
+
+    }
+}
